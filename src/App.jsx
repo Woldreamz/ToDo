b985cef2010a3876ee1578f1f1a9f69527f844bd
Woldreamz <woldreamz@icloud.com>
2024-10-18 04:20:42 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import TodoInput from "./components/TodoInput.jsx";
+import TodoList from "./components/TodoList.jsx";
+import React from "react";
+
+function App() {
+  return (
+    <main>
+      <TodoInput />
+      <TodoList />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const TodoInput = () => {
+    return (
+        <div>TodoInput</div>
+    )
+}
+export default TodoInput
