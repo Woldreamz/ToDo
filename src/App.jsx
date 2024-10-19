d745872806dad010c156b356f127c1fa7ff7c325
Woldreamz <woldreamz@icloud.com>
2024-10-19 08:42:12 +0100

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,54 @@
 import TodoInput from "./components/TodoInput.jsx";
 import TodoList from "./components/TodoList.jsx";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [todoValue, setTodoValue] = useState("");
+
+  function persistData(newList) {
+    localStorage.setItem("todos", JSON.stringify({ todos: newList }));
+  }
+
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo];
+    persistData(newTodoList);
+    setTodos(newTodoList);
+  }
+
+  function handleDeleteTodo(index) {
+    const newTodoList = todos.filter((todo, todoIndex) => {
+      return todoIndex !== index;
+    });
+    persistData(newTodoList);
+    setTodos(newTodoList);
+  }
+
+  function handleEditTodo(index) {
+    const valueToBeEdited = todos[index];
+    setTodoValue(valueToBeEdited);
+    handleDeleteTodo(index);
+  }
+
+  useEffect(() => {
+    if (!localStorage) {
+      return;
+    }
+
+    let localTodos = localStorage.getItem("todos");
+    if (localTodos) {
+      localTodos = JSON.parse(localTodos).todos;
+    }
+  }, []);
+
   return (
     <main>
-      <TodoInput />
-      <TodoList />
+      <TodoInput
+        handleAddTodos={handleAddTodos}
+        todoValue={todoValue}
+        setTodoValue={setTodoValue}
+      />
+      <TodoList handleDeleteTodo={handleDeleteTodo} todos={todos} />
     </main>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from "react";
+import TodoCard from "./TodoCard.jsx";
 
-const TodoList = () => {
-    return (
-        <div>TodoList</div>
-    )
-}
-export default TodoList
+const TodoList = (props) => {
+  const { todos } = props;
+
+  return (
+    <div>
+      {todos.map((todo, todoIndex) => {
+        return (
+          <TodoCard {...props} key={todoIndex} index={todoIndex}>
+            <p>{todo}</p>
+          </TodoCard>
+        );
+      })}
+    </div>
+  );
+};
+export default TodoList;
